cf5bf2db4733af8228c355577b18168883571891
salmanrf <salmanrf2@gmail.com>
2023-03-05 16:15:52 +0700

    refactor: disable react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,8 @@ const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </ChakraProvider>
 );
